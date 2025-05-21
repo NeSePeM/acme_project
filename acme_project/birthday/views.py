360f6efc0d505122d0e2cbabc1b2cba1de7d86f6
Maxim Datskovskiy <nspmax@ya.ru>
2025-05-22 02:34:27 +0300

    2.2.1

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,5 @@
 # birthday/views.py
-from django.shortcuts import get_object_or_404, redirect, render
+# from django.shortcuts import get_object_or_404, redirect, render
 # from django.core.paginator import Paginator
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 from .forms import BirthdayForm
 from .models import Birthday
 # Импортируем из utils.py функцию для подсчёта дней.
-from .utils import calculate_birthday_countdown
+# from .utils import calculate_birthday_countdown
 
 
 class BirthdayMixin:
@@ -15,17 +15,12 @@ class BirthdayMixin:
     success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayFormMixin:
+class BirthdayCreateView(BirthdayMixin, CreateView):
     form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
 
 
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    pass
-
-
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    pass
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    form_class = BirthdayForm
 
 
 class BirthdayDeleteView(BirthdayMixin, DeleteView):
